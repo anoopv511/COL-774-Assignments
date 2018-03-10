efcb28f99ab2e96a214012d5ffa2a3655963dacc
Anoop <511anoopv@gmail.com>
2018-03-11 03:40:47 +0530

    Finished SVM implementation

--- Assignment 2/src/mysvm.py
+++ Assignment 2/src/mysvm.py
@@ -2,19 +2,7 @@ import numpy as np
 import itertools
 import matplotlib.pyplot as plt
 from scipy import stats
-
-# Train Data
-x_train = np.loadtxt('../data/mnist/train.csv',delimiter=',',dtype=np.float64)
-y_train = x_train[:,-1].reshape(-1,1)
-x_train = np.delete(x_train,-1,1)
-train_max, train_min = x_train.max(), x_train.min()
-x_train /= (train_max - train_min)              # Scaling
-
-# Test Data
-x_test = np.loadtxt('../data/mnist/test.csv',delimiter=',',dtype=np.float64)
-y_test = x_test[:,-1].reshape(-1,1)
-x_test = np.delete(x_test,-1,1)
-x_test /= (train_max - train_min)               # Scaling
+from functions import *
 
 ############ Part (a) ############
 
@@ -73,7 +61,7 @@ def onevsone(x_train,y_train,maxit,maxcount,batchsize,lambda_,c=1,project=False,
         idx = np.concatenate((idx_c1,idx_c2))
         sub_x, sub_y = x_train[idx], y_train[idx]
         sub_y = (sub_y == split[0]).astype(np.int64) - (sub_y == split[1]).astype(np.int64)
-        classifier = SVM(maxit,batchsize,lambda_,c)
+        classifier = SVM(maxit,maxcount,batchsize,lambda_,c)
         classifier.fit(sub_x,sub_y,printAfter)
         classifiers.append((classifier,split[0],split[1]))
     return classifiers
@@ -99,30 +87,13 @@ def indv_acc_onevsone(classifiers,x_test,y_test):
         pred = np.where(pred == 1,c[1]*ones,c[2]*ones)
         print("Accuracy for classifier b/w {0}/{1} = {2}".format(c[1],c[2],(pred == sub_y).sum()/float(sub_y.shape[0])))
 
-svm_one = onevsone(x_train,y_train,5000,5,100,1,1,False,10000)
-
-# Train Accuracy
-pred_one_train = pred_onevsone(svm_one,x_train)
-accuracy_one_train = (pred_one_train == y_train).sum()/float(y_train.shape[0])
-print("One-vs-One Train Accuracy = {0}".format(accuracy_one_train))
-# indv_acc_onevsone(svm_one,x_train,y_train)
-
-# Test Accuracy
-pred_one_test = pred_onevsone(svm_one,x_test)
-accuracy_one_test = (pred_one_test == y_test).sum()/float(y_test.shape[0])
-print("One-vs-One Test Accuracy = {0}".format(accuracy_one_test))
-# indv_acc_onevsone(svm_one,x_test,y_test)
-
-# One-vs-One Train Accuracy = 0.8871
-# One-vs-One Test Accuracy = 0.8986
-
 # One-vs-All Model
 def onevsall(x_train,y_train,maxit,maxcount,batchsize,lambda_,c=1,project=False,printAfter=1):
     classifiers = []
     labels = np.sort(np.unique(y_train))
     for l in labels:
         sub_x, sub_y = x_train, (y_train == l).astype(np.int64) - (y_train != l).astype(np.int64)
-        classifier = SVM(maxit,batchsize,lambda_,c)
+        classifier = SVM(maxit,maxcount,batchsize,lambda_,c)
         classifier.fit(sub_x,sub_y,printAfter)
         classifiers.append((classifier,l))
     return classifiers
@@ -142,21 +113,53 @@ def indv_acc_onevsall(classifiers,x_test,y_test):
         sub_y = np.where(y_test == c[1],c[1]*ones,-1*ones)
         print("Accuracy for classifier b/w {0}/Rest = {1}".format(c[1],(pred == sub_y).sum()/float(sub_y.shape[0])))
 
-svm_all = onevsall(x_train,y_train,5000,5,100,1,1,False,10000)
+if __name__ == "__main__":
+    # Train Data
+    x_train = np.loadtxt('../data/mnist/train.csv',delimiter=',',dtype=np.float64)
+    y_train = x_train[:,-1].reshape(-1,1)
+    x_train = np.delete(x_train,-1,1)
+    x_train /= 255              # Scaling
 
-# Train Accuracy
-pred_all_train = pred_onevsall(svm_all,x_train)
-accuracy_all_train = (pred_all_train == y_train).sum()/float(y_train.shape[0])
-print("One-vs-All Train Accuracy = {0}".format(accuracy_all_train))
-# indv_acc_onevsall(svm_all,x_train,y_train)
+    # Test Data
+    x_test = np.loadtxt('../data/mnist/test.csv',delimiter=',',dtype=np.float64)
+    y_test = x_test[:,-1].reshape(-1,1)
+    x_test = np.delete(x_test,-1,1)
+    x_test /= 255               # Scaling
 
-# Test Accuracy
-pred_all_test = pred_onevsall(svm_all,x_test)
-accuracy_all_test = (pred_all_test == y_test).sum()/float(y_test.shape[0])
-print("One-vs-All Test Accuracy = {0}".format(accuracy_all_test))
-# indv_acc_onevsall(svm_all,x_test,y_test)
+    svm_one = onevsone(x_train,y_train,5000,7,100,0.05,1,False,10000)
+    save(svm_one,"../data/tmp/svm_one.pickle")
 
-# One-vs-All Train Accuracy = 0.77605
-# One-vs-All Test Accuracy = 0.7842
+    # Train Accuracy
+    pred_one_train = pred_onevsone(svm_one,x_train)
+    accuracy_one_train = (pred_one_train == y_train).sum()/float(y_train.shape[0])
+    print("One-vs-One Train Accuracy = {0}".format(accuracy_one_train))
+    # indv_acc_onevsone(svm_one,x_train,y_train)
+
+    # Test Accuracy
+    pred_one_test = pred_onevsone(svm_one,x_test)
+    accuracy_one_test = (pred_one_test == y_test).sum()/float(y_test.shape[0])
+    print("One-vs-One Test Accuracy = {0}".format(accuracy_one_test))
+    # indv_acc_onevsone(svm_one,x_test,y_test)
+
+    # One-vs-One Train Accuracy = 0.9341
+    # One-vs-One Test Accuracy = 0.9328
+
+    svm_all = onevsall(x_train,y_train,5000,7,100,0.05,1,False,10000)
+    save(svm_all,"../data/tmp/svm_all.pickle")
+
+    # Train Accuracy
+    pred_all_train = pred_onevsall(svm_all,x_train)
+    accuracy_all_train = (pred_all_train == y_train).sum()/float(y_train.shape[0])
+    print("One-vs-All Train Accuracy = {0}".format(accuracy_all_train))
+    # indv_acc_onevsall(svm_all,x_train,y_train)
+
+    # Test Accuracy
+    pred_all_test = pred_onevsall(svm_all,x_test)
+    accuracy_all_test = (pred_all_test == y_test).sum()/float(y_test.shape[0])
+    print("One-vs-All Test Accuracy = {0}".format(accuracy_all_test))
+    # indv_acc_onevsall(svm_all,x_test,y_test)
+
+    # One-vs-All Train Accuracy = 0.87715
+    # One-vs-All Test Accuracy = 0.887
 
 ##################################
