eb12b098b120ff9a52d6be4ec98ab79e9d7ec531
Anoop <511anoopv@gmail.com>
2018-04-10 19:25:41 +0530

    Finished Part 1a,1b,1c of Assignment 3

--- Assignment 3/src/dtree.py
+++ Assignment 3/src/dtree.py
@@ -31,8 +31,8 @@ class Node():
         self.pred = pred
         self.parent = None
         self.children = []
-        self.mispred = ()
-        self.prune_children_cost = 0
+        self.n_mis = []
+        self.mis_pred = 0
 
     def get_split(self,x):
         return [np.where(c(x[:,self.col]))[0] for c in self.cond] if self.col != -1 else [np.arange(x.shape[0])]
@@ -45,7 +45,7 @@ class DTree():
         self.root = None
         self.node_count = 0
 
-    def growTree(self,x_train,y_train,median_split=False):
+    def growTree(self,x_train,y_train,init_median_split=True,num_cols=[]):
         self.node_count += 1
         if(y_train.sum() == 0):
             self.root = Node(self.node_count,None,-1,0)
@@ -54,13 +54,17 @@ class DTree():
             self.root = Node(self.node_count,None,-1,1)
             return
         else:
-            xjs, col = self.choose_best_attr(x_train,y_train)
+            xjs, col, med_split = self.choose_best_attr(x_train,y_train,init_median_split,num_cols)
             c1 = np.count_nonzero(y_train)
             c0 = y_train.shape[0] - c1
             if(xjs is None):
                 self.root = Node(None,-1,np.argmax([c0,c1]))
                 return
-            conds = [(lambda x,c=c: x == c) for c in xjs]           
+            if(not med_split):
+                conds = [(lambda x,c=c: x == c) for c in xjs]
+            else:
+                med = xjs[0]
+                conds = [(lambda x: x >= med),(lambda x: x < med)]
             self.root = Node(conds,col,np.argmax([c0,c1]))
             nodes = deque([self.root])
             indices = deque([self.root.get_split(x_train)])
@@ -78,14 +82,18 @@ class DTree():
                         curr.children[-1].parent = curr
                         continue
                     else:
-                        xjs, col = self.choose_best_attr(x_train[idx],y_train[idx])
+                        xjs, col, med_split = self.choose_best_attr(x_train[idx],y_train[idx],init_median_split,num_cols)
                         c1 = np.count_nonzero(y_train[idx])
                         c0 = y_train[idx].shape[0] - c1
                         if(xjs is None):
                             curr.children.append(Node(None,-1,np.argmax([c0,c1])))
                             curr.children[-1].parent = curr
                             continue
-                        conds = [(lambda x,c=c: x == c) for c in xjs]
+                        if(not med_split):
+                            conds = [(lambda x,c=c: x == c) for c in xjs]
+                        else:
+                            med = xjs[0]
+                            conds = [(lambda x,med=med: x >= med),(lambda x,med=med: x < med)]
                         node = Node(conds,col,np.argmax([c0,c1]))
                         curr.children.append(node)
                         curr.children[-1].parent = curr
@@ -93,27 +101,44 @@ class DTree():
                         split = node.get_split(x_train[idx])
                         indices.append([idx[s] for s in split])
 
-    def choose_best_attr(self,x_train,y_train):
+    def choose_best_attr(self,x_train,y_train,init_median_split,num_cols):
         xjss, info_loss = [], []
         for col in range(x_train.shape[1]):
-            xjs = np.unique(x_train[:,col])
-            if(xjs.shape[0] == 1):
-                info_loss.append(100)
+            flag = True if init_median_split else not col in num_cols
+            if(flag):
+                xjs = np.unique(x_train[:,col])
                 xjss.append(xjs)
-                continue
-            xjss.append(xjs)
-            P_xj = [(x_train[:,col] == xj).sum()/x_train.shape[0] for xj in xjs]
+                if(xjs.shape[0] == 1):
+                    info_loss.append(100)
+                    continue
+            else:
+                med = np.median(x_train[:,col])
+                mod_col = (x_train[:,col] >= med).astype(np.int64)
+                xjss.append([med])
+                mod_col_sum = mod_col.sum()
+                if(mod_col_sum == 0 or mod_col_sum == x_train.shape[0]):
+                    info_loss.append(100)
+                    continue
+                xjs = np.array([0,1])
+            if(flag):
+                P_xj = [(x_train[:,col] == xj).sum()/x_train.shape[0] for xj in xjs]
+            else:
+                P_xj = [(mod_col == xj).sum()/x_train.shape[0] for xj in xjs]
             ys = np.unique(y_train)
             H_y_xj = np.zeros(xjs.shape)
             for idx, xj in enumerate(xjs):
                 P_y_xj = np.zeros(ys.shape)
                 for idy, y in enumerate(ys):
-                    P_y_xj[idy] = ((x_train[:,col] == xj)*(y_train == y)).sum()/(y_train == y).sum()
+                    if(flag):
+                        P_y_xj[idy] = ((x_train[:,col] == xj)*(y_train == y)).sum()/(x_train[:,col] == xj).sum()
+                    else:
+                        P_y_xj[idy] = ((mod_col == xj)*(y_train == y)).sum()/(mod_col == xj).sum()
                 H_y_xj[idx] = -1*(P_y_xj*np.log(P_y_xj + (P_y_xj == 0))).sum()
             info_loss.append((P_xj*H_y_xj).sum())
         best_col = np.argmin(info_loss)
-        if(info_loss[best_col] == 100): return None, None
-        return xjss[best_col], best_col
+        if(info_loss[best_col] == 100): return None, None, None
+        med_split = False if init_median_split else (best_col in num_cols)
+        return xjss[best_col], best_col, med_split
     
     def predict(self,x_test):
         def rec_predict(node,x_test,idx,pred):
@@ -126,14 +151,19 @@ class DTree():
         rec_predict(self.root,x_test,np.arange(x_test.shape[0]),pred)
         return pred
     
-    def dfs_acc(self,x_test,y_test,label_mispred=False):
+    def dfs_acc(self,x_test,y_test,label_mis=False):
         def rec_dfs(node,x_test,y_test,idx,pred,acc):
             pred[idx] = node.pred
             acc.append((pred == y_test).sum()/y_test.shape[0])
-            if(label_mispred):
-                curr.mispred = (idx.shape[0],(pred[idx] != y_test[idx]).sum())
+            if(label_mis):
+                curr.n_mis = [(y_test[idx] == 0).sum(),(y_test[idx] == 1).sum(),(pred[idx] != y_test[idx]).sum()]
             if(not node.is_leaf()):
                 split = node.get_split(x_test)
+                if(label_mis):
+                    oi = np.ones(len(idx),dtype=bool)
+                    for i in split:
+                        oi[i] = False
+                    curr.n_mis[-1] = (pred[idx[oi]] != y_test[idx[oi]]).sum()
                 for c, i in zip(node.children,split):
                     rec_dfs(c,x_test[i],y_test,idx[i],pred,acc)
         pred = np.ones(x_test.shape[0],dtype=np.int64) * -1
@@ -141,7 +171,7 @@ class DTree():
         rec_dfs(self.root,x_test,y_test,np.arange(x_test.shape[0]),pred,acc)
         return np.array(acc)
     
-    def bfs_acc(self,x_test,y_test,label_mispred=False):
+    def bfs_acc(self,x_test,y_test,label_mis=False):
         pred = np.ones(x_test.shape[0],dtype=np.int64) * -1
         acc = []
         nodes = deque([self.root])
@@ -153,21 +183,26 @@ class DTree():
             x = data.popleft()
             pred[idx] = curr.pred
             acc.append((pred == y_test).sum()/y_test.shape[0])
-            if(label_mispred):
-                curr.mispred = (idx.shape[0],(pred[idx] != y_test[idx]).sum())
+            if(label_mis):
+                curr.n_mis = [(y_test[idx] == 0).sum(),(y_test[idx] == 1).sum(),(pred[idx] != y_test[idx]).sum()]
             if(not curr.is_leaf()):
                 split = curr.get_split(x)
                 nodes.extend(curr.children)
                 indices.extend([idx[i] for i in split])
                 data.extend([x[i] for i in split])
+                if(label_mis):
+                    oi = np.ones(len(idx),dtype=bool)
+                    for i in split:
+                        oi[i] = False
+                    curr.n_mis[-1] = (pred[idx[oi]] != y_test[idx[oi]]).sum()
         return np.array(acc)
     
-    def get_nodes(self,reset_prune_cost=False):
+    def get_nodes(self,reset_mispred=False):
         nodes = []
         node_count = [0]
         def rec_get_nodes(node,depth,node_count):
             node_count[0] += 1
-            if(reset_prune_cost): node.prune_children_cost = 0
+            if(reset_mispred): node.mis_pred = node.n_mis[-1]
             if(len(nodes) < depth + 1):
                 nodes.append([node])
             else:
@@ -179,28 +214,28 @@ class DTree():
         return nodes
         
     def post_prune(self,x_valid,y_valid,x_data,y_data,verbose=False):
-        if(len(self.root.mispred) == 0): self.bfs_acc(x_valid,y_valid,label_mispred=True)
+        if(len(self.root.n_mis) == 0): self.bfs_acc(x_valid,y_valid,label_mis=True)
         def rec_prune():
-            nodes = self.get_nodes(reset_prune_cost=True)
+            nodes = self.get_nodes(reset_mispred=True)
             for sub_n in nodes[::-1]:
                 for n in sub_n:
                     if(not n.parent is None):
-                        if(n.parent.pred != n.pred):
-                            n.parent.prune_children_cost += n.mispred[0] - 2*n.mispred[1]
+                        n.parent.mis_pred += n.mis_pred
             best_node, prune_cost = None, 0
             for sub_n in nodes:
                 for n in sub_n:
-                    if(n.prune_children_cost < prune_cost):
-                        best_node, prune_cost = n, n.prune_children_cost
-            if(prune_cost != 0):
+                    prune_mis = n.n_mis[0] if n.pred == 1 else n.n_mis[1]
+                    no_prune_mis = n.mis_pred
+                    curr_prune_cost = prune_mis - no_prune_mis
+                    if(curr_prune_cost < prune_cost and not n.is_leaf()):
+                        best_node, prune_cost = n, curr_prune_cost
+            if(not best_node is None):
                 best_node.children = []
+                best_node.n_mis[-1] = prune_mis
                 return 0
             else:
                 return -1
-        node_counts = []
-        train_acc = []
-        valid_acc = []
-        test_acc = []
+        node_counts, train_acc, valid_acc, test_acc = [], [], [], []
         p1, p2 = x_train.shape[0], x_train.shape[0]+x_valid.shape[0]
         while(rec_prune() == 0):
             node_counts.append(self.node_count)
@@ -216,7 +251,7 @@ class DTree():
 ############ Part (a) ############
 
 tree = DTree()
-tree.growTree(x_train,y_train)
+tree.growTree(x_train,y_train,init_median_split=True)
 
 print("Number of Nodes = {0}".format(tree.node_count))
 p_train = tree.predict(x_train)
@@ -226,10 +261,10 @@ print("Validation Accuracy = {0:.5f}".format((p_valid == y_valid).sum()/y_valid.
 p_test = tree.predict(x_test)
 print("Test Accuracy = {0:.5f}".format((p_test == y_test).sum()/y_test.shape[0]))
 
-# Number of Nodes = 8369
+# Number of Nodes = 7972
 # Train Accuracy = 0.88933
-# Validation Accuracy = 0.80200
-# Test Accuracy = 0.80657
+# Validation Accuracy = 0.79933
+# Test Accuracy = 0.80671
 
 train_acc = tree.bfs_acc(x_train,y_train)
 valid_acc = tree.bfs_acc(x_valid,y_valid)
@@ -267,7 +302,7 @@ plt.show()
 
 x_data = np.concatenate((x_train,x_valid,x_test),axis=0)
 y_data = np.concatenate((y_train,y_valid,y_test),axis=0)
-node_counts, train_acc, valid_acc, test_acc = tree.post_prune(x_valid,y_valid,x_data,y_data)
+node_counts, train_acc, valid_acc, test_acc = tree.post_prune(x_valid,y_valid,x_data,y_data,verbose=False)
 
 print("Number of Nodes = {0}".format(tree.node_count))
 p_train = tree.predict(x_train)
@@ -277,10 +312,119 @@ print("Validation Accuracy = {0:.5f}".format((p_valid == y_valid).sum()/y_valid.
 p_test = tree.predict(x_test)
 print("Test Accuracy = {0:.5f}".format((p_test == y_test).sum()/y_test.shape[0]))
 
-# Number of Nodes = 6509
-# Train Accuracy = 0.86274
-# Validation Accuracy = 0.84067
-# Test Accuracy = 0.80957
+# Number of Nodes = 4328
+# Train Accuracy = 0.85789
+# Validation Accuracy = 0.83800
+# Test Accuracy = 0.81871
+
+sns.set()
+fig = plt.figure(figsize=(7,5))
+train_patch = plt.plot(node_counts,train_acc,color='b',label='Train Accuracy')[0]
+valid_patch = plt.plot(node_counts,valid_acc,color='g',label='Validation Accuracy')[0]
+test_patch = plt.plot(node_counts,test_acc,color='r',label='Test Accuracy')[0]
+plt.xlim(node_counts.max()+100,node_counts.min()-100)
+plt.xlabel("Number of Nodes")
+plt.ylabel("Accuracy")
+plt.legend(handles=[train_patch,valid_patch,test_patch])
+plt.title("Accuracy vs Number of Nodes (Pruning)")
+plt.show()
+
+##################################
+
+############ Part (c) ############
+
+x_train = train.iloc[:,:-1]
+x_valid = valid.iloc[:,:-1]
+x_test = test.iloc[:,:-1]
+
+encodings = get_encodings(x_train,median_spit=False)
+x_train = preprocess(x_train,encodings)
+columns = x_train.columns
+num_cols = [np.argmax(columns == c) for c in list(set(columns) - set(encodings))]
+x_train = x_train.as_matrix()
+x_valid = preprocess(x_valid,encodings).as_matrix()
+x_test = preprocess(x_test,encodings).as_matrix()
+
+tree = DTree()
+tree.growTree(x_train,y_train,init_median_split=False,num_cols=num_cols)
+
+print("Number of Nodes = {0}".format(tree.node_count))
+p_train = tree.predict(x_train)
+print("Train Accuracy = {0:.5f}".format((p_train == y_train).sum()/y_train.shape[0]))
+p_valid = tree.predict(x_valid)
+print("Validation Accuracy = {0:.5f}".format((p_valid == y_valid).sum()/y_valid.shape[0]))
+p_test = tree.predict(x_test)
+print("Test Accuracy = {0:.5f}".format((p_test == y_test).sum()/y_test.shape[0]))
+
+# Number of Nodes = 16495
+# Train Accuracy = 0.99989
+# Validation Accuracy = 0.78800
+# Test Accuracy = 0.77914
+
+train_acc = tree.bfs_acc(x_train,y_train)
+valid_acc = tree.bfs_acc(x_valid,y_valid)
+test_acc = tree.bfs_acc(x_test,y_test)
+
+sns.set()
+fig = plt.figure(figsize=(7,5))
+train_patch = plt.plot(np.arange(1,tree.node_count+1),train_acc,color='b',label='Train Accuracy')[0]
+valid_patch = plt.plot(np.arange(1,tree.node_count+1),valid_acc,color='g',label='Validation Accuracy')[0]
+test_patch = plt.plot(np.arange(1,tree.node_count+1),test_acc,color='r',label='Test Accuracy')[0]
+plt.xlabel("Number of Nodes")
+plt.ylabel("Accuracy")
+plt.legend(handles=[train_patch,valid_patch,test_patch])
+plt.title("Accuracy vs Number of Nodes (BFS)")
+plt.show()
+
+train_acc = tree.dfs_acc(x_train,y_train)
+valid_acc = tree.dfs_acc(x_valid,y_valid)
+test_acc = tree.dfs_acc(x_test,y_test)
+
+sns.set()
+fig = plt.figure(figsize=(7,5))
+train_patch = plt.plot(np.arange(1,tree.node_count+1),train_acc,color='b',label='Train Accuracy')[0]
+valid_patch = plt.plot(np.arange(1,tree.node_count+1),valid_acc,color='g',label='Validation Accuracy')[0]
+test_patch = plt.plot(np.arange(1,tree.node_count+1),test_acc,color='r',label='Test Accuracy')[0]
+plt.xlabel("Number of Nodes")
+plt.ylabel("Accuracy")
+plt.legend(handles=[train_patch,valid_patch,test_patch])
+plt.title("Accuracy vs Number of Nodes (DFS)")
+plt.show()
+
+nodes = tree.get_nodes()
+counts = dict(zip(num_cols,[0]*len(num_cols)))
+for sub_n in nodes:
+    for n in sub_n:
+        try:
+            counts[n.col] += 1
+        except KeyError:
+            continue
+for c in counts:
+    print("{0} - {1}".format(columns[c],counts[c]))
+
+# Age - 3446
+#  Fnlwgt - 2194
+#  Education Number - 0
+#  Capital Gain - 18
+#  Capital Loss - 6
+#  Hour per Week - 536
+
+x_data = np.concatenate((x_train,x_valid,x_test),axis=0)
+y_data = np.concatenate((y_train,y_valid,y_test),axis=0)
+node_counts, train_acc, valid_acc, test_acc = tree.post_prune(x_valid,y_valid,x_data,y_data,verbose=False)
+
+print("Number of Nodes = {0}".format(tree.node_count))
+p_train = tree.predict(x_train)
+print("Train Accuracy = {0:.5f}".format((p_train == y_train).sum()/y_train.shape[0]))
+p_valid = tree.predict(x_valid)
+print("Validation Accuracy = {0:.5f}".format((p_valid == y_valid).sum()/y_valid.shape[0]))
+p_test = tree.predict(x_test)
+print("Test Accuracy = {0:.5f}".format((p_test == y_test).sum()/y_test.shape[0]))
+
+# Number of Nodes = 3650
+# Train Accuracy = 0.87230
+# Validation Accuracy = 0.84167
+# Test Accuracy = 0.81429
 
 sns.set()
 fig = plt.figure(figsize=(7,5))
